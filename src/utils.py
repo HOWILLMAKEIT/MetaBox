d16e8e68d9e6054c41c2bd6c191668702a16d16c
五彩湾刺猬 <wangchen0339@gmail.com>
2025-03-15 15:09:55 +0800

    add some configs and the loading of  dataset

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
 from problem import bbob, bbob_torch, protein_docking
-
+from problem.SOO.self_generated.Symbolic_bench_numpy.Symbolic_bench_Dataset import Symbolic_bench_Dataset
+from problem.SOO.self_generated.Symbolic_bench_torch.Symbolic_bench_Dataset import Symbolic_bench_Dataset_torch
 
 def construct_problem_set(config):
     problem = config.problem
@@ -23,5 +24,15 @@ def construct_problem_set(config):
                                                                     train_batch_size=config.train_batch_size,
                                                                     test_batch_size=config.test_batch_size,
                                                                     difficulty=config.difficulty)
+    elif problem in ['Symbolic_bench','Symbolic_bench-torch']:
+        if problem == 'Symbolic_bench':
+            return Symbolic_bench_Dataset.get_datasets(upperbound=config.upperbound,    
+                                                       train_batch_size=config.train_batch_size,
+                                                          test_batch_size=config.test_batch_size)
+        else:            
+            return Symbolic_bench_Dataset_torch.get_datasets(upperbound=config.upperbound,    
+                                                       train_batch_size=config.train_batch_size,
+                                                          test_batch_size=config.test_batch_size)
+            
     else:
         raise ValueError(problem + ' is not defined!')
