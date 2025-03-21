5786af4ee2efa92423f04c22bfb2e2e0d7ee6125
HuangZY <3153580592@qq.com>
2025-03-21 15:17:24 +0800

    Update bbob_surrogate.py

--- src/problem/SOO/bbob_surrogate/bbob_surrogate.py
+++ src/problem/SOO/bbob_surrogate/bbob_surrogate.py
@@ -5,6 +5,7 @@ from problem.SOO.bbob_numpy.bbob import *
 from os import path
 from torch.utils.data import Dataset
 
+
 class bbob_surrogate_model(Basic_Problem):
 	def __init__(self, dim, func_id, lb, ub, shift, rotate, bias, config):
 		self.dim = dim
@@ -16,14 +17,49 @@ class bbob_surrogate_model(Basic_Problem):
 
 		base_dir = path.dirname(path.dirname(path.dirname(path.abspath(__file__))))
 
-		if func_id in [1, 3, 4, 6, 7, 10, 12, 13, 14, 15, 23, 24]:
-			self.model = KAN.loadckpt(path.join(base_dir, f'datafiles\\SOO\\surrogate_model\\Dim{dim}\\KAN\\{self.instance}\\model'))
-			# self.model = KAN.loadckpt(f'./problem/data_files/surrogate_model/Dim{dim}/KAN/{self.instance}/model')
-		elif func_id in [2, 5, 8, 9, 11, 16, 17, 18, 19, 20, 21, 22]:
-			self.model = MLP(dim)
-			self.model.load_state_dict(
-				torch.load(path.join(base_dir, f'datafiles\\SOO\\surrogate_model\\Dim{dim}\\MLP\\{self.instance}\\model.pth'))
-			)
+		if dim == 2:
+
+			if func_id in [1, 6, 8, 9, 12, 14, 19, 20, 23]:
+				self.model = KAN.loadckpt(
+					path.join(base_dir, f'datafiles\\SOO\\surrogate_model\\Dim{dim}\\KAN\\{self.instance}\\model'))
+			# elif func_id in [2, 3, 4, 5, 7, 10, 11, 13, 15, 16, 17, 18, 21, 22, 23]:
+			else:
+				self.model = MLP(dim)
+				self.model.load_state_dict(
+					torch.load(path.join(base_dir,
+										 f'datafiles\\SOO\\surrogate_model\\Dim{dim}\\MLP\\{self.instance}\\model.pth'))
+				)
+
+
+		elif dim == 5:
+
+			if func_id in [1, 2, 4, 6, 8, 9, 11, 12, 14, 20, 23]:
+				self.model = KAN.loadckpt(
+					path.join(base_dir, f'datafiles\\SOO\\surrogate_model\\Dim{dim}\\KAN\\{self.instance}\\model'))
+			else:
+				self.model = MLP(dim)
+				self.model.load_state_dict(
+					torch.load(path.join(base_dir,
+										 f'datafiles\\SOO\\surrogate_model\\Dim{dim}\\MLP\\{self.instance}\\model.pth'))
+				)
+
+		elif dim == 10:
+
+			if func_id in [1, 2, 4, 6, 9, 12, 14, 23]:
+				self.model = KAN.loadckpt(
+					path.join(base_dir, f'datafiles\\SOO\\surrogate_model\\Dim{dim}\\KAN\\{self.instance}\\model'))
+			# elif func_id in [2, 5, 8, 9, 11, 16, 17, 18, 19, 20, 21, 22]:
+			else:
+				self.model = MLP(dim)
+				self.model.load_state_dict(
+					torch.load(path.join(base_dir,
+										 f'datafiles\\SOO\\surrogate_model\\Dim{dim}\\MLP\\{self.instance}\\model.pth'))
+				)
+
+
+		else:
+			raise ValueError(f'training on dim{dim} is not supported yet.')
+
 		self.model.to(self.device)
 		# KAN: 1,3,4,6,7,10,12,13,14,15,23,24  MLP:2,5,8,9,11,16,17,18,19,20,21,22
 
@@ -33,13 +69,21 @@ class bbob_surrogate_model(Basic_Problem):
 	def func(self, x):
 		if isinstance(x, np.ndarray):
 			x = torch.tensor(x).to(self.device)
-		input_x = (x - self.lb) / (self.ub - self.lb)
-		input_x = input_x.to(torch.float32)
-		with torch.no_grad():
-			y = self.model(input_x)
+			input_x = (x - self.lb) / (self.ub - self.lb)
+			input_x = input_x.to(torch.float32)
+			with torch.no_grad():
+				y = self.model(input_x)
 
-		return y.flatten().cpu().numpy()
-		# return y
+			return y.flatten().cpu().numpy()
+
+		elif isinstance(x, torch.tensor):
+			input_x = (x - self.lb) / (self.ub - self.lb)
+			input_x = input_x.to(torch.float32)
+			with torch.no_grad():
+				y = self.model(input_x)
+			return y
+
+	# return y
 
 	def __str__(self):
 		return f'Surrogate_{self.instance}'
@@ -58,26 +102,30 @@ class bbob_surrogate_Dataset(Dataset):
 
 	@staticmethod
 	def get_datasets(dim, config, upperbound=5, train_batch_size=1,
-					 test_batch_size=1, difficulty='easy',seed=3849,
+					 test_batch_size=1, difficulty='easy', seed=3849,
 					 shifted=True, biased=True, rotated=True):
 
 		is_train = config.is_train
-		
+
 		# train_id = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
 		# 			20]
 		# test_id = [16, 17, 18, 19, 21, 22, 23, 24]
 
 		if difficulty == 'easy':
-			train_id = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
-					20]
-			# test_id = [16, 17, 18, 19, 21, 22, 23, 24]
+			if dim == 2:
+				train_id = [1, 2, 3, 4, 5, 6, 8, 9, 10, 11, 12, 13, 14, 15,
+							20, 22]
+			elif dim == 5 or dim == 10:
+				train_id = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+							20]
+		# test_id = [16, 17, 18, 19, 21, 22, 23, 24]
 		elif difficulty == 'difficult':
-			train_id = [1, 2, 4, 5, 6, 10, 11, 13]
+			if dim == 2 or dim == 5:
+				train_id = [1, 2, 5, 6, 10, 11, 13, 14]
+			elif dim == 10:
+				train_id = [1, 2, 5, 6, 10, 11, 13, 20]
 		else:
 			raise ValueError(f'{difficulty} difficulty is invalid.')
-
-		if dim not in[2, 5, 10] and config.is_train:
-			raise ValueError(f'dim{dim} is not supported yet.')
 
 		np.random.seed(seed)
 		train_set = []
@@ -100,13 +148,13 @@ class bbob_surrogate_Dataset(Dataset):
 			else:
 				bias = 0
 			if id in train_id:
-			
+
 				if is_train:
 					train_instance = bbob_surrogate_model(dim, id, ub=ub, lb=lb, shift=shift, rotate=H, bias=bias,
-												config=config)
+														  config=config)
 				else:
 					train_instance = eval(f'F{id}')(dim=dim, shift=shift, rotate=H, bias=bias, lb=lb, ub=ub)
-				
+
 				train_set.append(train_instance)
 
 			# if id in test_id:
