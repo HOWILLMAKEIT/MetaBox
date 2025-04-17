b66350def6774d044858c9e25a82098e5298d47f
WenJie Qiu <100810925+Wukong-SCUT@users.noreply.github.com>
2025-04-17 09:12:54 +0800

    Update cec2013lsgo_dataset.py

--- src/environment/problem/SOO/CEC2013LSGO/cec2013lsgo_dataset.py
+++ src/environment/problem/SOO/CEC2013LSGO/cec2013lsgo_dataset.py
@@ -11,6 +11,9 @@ class CEC2013LSGO_Dataset(Dataset):
         self.N = len(self.data)
         self.ptr = [i for i in range(0, self.N, batch_size)]
         self.index = np.arange(self.N)
+        self.maxdim = 0
+        for item in self.data:
+            self.maxdim = max(self.maxdim, item.dim)
 
     @staticmethod
     def get_datasets(version='numpy',
