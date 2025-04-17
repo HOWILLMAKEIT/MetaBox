9d29b20321adc89c88a7372e84d884c349941dbb
Feather_red <1328181376@qq.com>
2025-04-17 20:35:30 +0800

    update GLEET

--- src/baseline/metabbo/gleet.py
+++ src/baseline/metabbo/gleet.py
@@ -258,7 +258,7 @@ class GLEET(PPO_Agent):
 
         state = env.reset()
         try:
-            state = torch.FloatTensor(state).to(self.device)
+            state = torch.Tensor(state).to(self.device)
         except:
             pass
 
@@ -293,7 +293,7 @@ class GLEET(PPO_Agent):
 
                 # state transient
                 state, rewards, is_end, info = env.step(action.cpu().numpy().squeeze())
-                memory.rewards.append(torch.FloatTensor(rewards).to(self.device))
+                memory.rewards.append(torch.Tensor(rewards).to(self.device))
                 # print('step:{},max_reward:{}'.format(t,torch.max(rewards)))
                 _R += rewards
                 # store info
@@ -302,7 +302,7 @@ class GLEET(PPO_Agent):
                 t = t + 1
 
                 try:
-                    state = torch.FloatTensor(state).to(self.device)
+                    state = torch.Tensor(state).to(self.device)
                 except:
                     pass
 
@@ -457,7 +457,7 @@ class GLEET(PPO_Agent):
         env.seed(seeds)
         state = env.reset()
         try:
-            state = torch.FloatTensor(state).to(self.device)
+            state = torch.Tensor(state).to(self.device)
         except:
             pass
 
@@ -471,10 +471,10 @@ class GLEET(PPO_Agent):
             # state transient
             state, rewards, is_end, info = env.step(action.cpu().numpy().squeeze())
             # print('step:{},max_reward:{}'.format(t,torch.max(rewards)))
-            R += torch.FloatTensor(rewards).squeeze()
+            R += torch.Tensor(rewards).squeeze()
             # store info
             try:
-                state = torch.FloatTensor(state).to(self.device)
+                state = torch.Tensor(state).to(self.device)
             except:
                 pass
         _Rs = R.detach().numpy().tolist()
@@ -496,7 +496,7 @@ class GLEET(PPO_Agent):
             R = 0
             while not is_done:
                 try:
-                    state = torch.FloatTensor(state).unsqueeze(0).to(self.device)
+                    state = torch.Tensor(state).unsqueeze(0).to(self.device)
                 except:
                     state = [state]
                 action = self.actor(state.double())[0]
