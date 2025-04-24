55c315a0555ce8d6b195354add7cd10e33f1588f
hohq <1984570111@qq.com>
2025-04-24 14:21:49 +0800

    [feature] a very raw origin code source presentation feature

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,11 +27,41 @@ release = 'v0.0.1'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
+    'sphinx.ext.linkcode',
     'autodoc2',
+    # 'sphinx.ext.viewcode',
     'sphinx_markdown_tables',
-    'myst_parser',
+    # 'myst_parser',
+    "myst_nb",
     'sphinx_copybutton',
+    
 ]
+viewcode_follow_imported_members = True
+viewcode_enable_epub = True
+autodoc2_config = {
+    "add_module_names": True,
+    "members": True,
+    "undoc-members": True,
+    "show-inheritance": True,
+    "inherited-members": "base",
+    "recursive": True,
+    "special-members": "__init__",
+    # "linkcode_resolve": True,  # 启用链接到源码
+    # "linkcode": True,  # 启用链接到源码
+}
+def linkcode_resolve(domain, info):
+    if domain != 'py':
+        return None
+    if not info['module']:
+        return None
+    
+    # 尝试查找源文件
+    module = info['module']
+    fullname = info['fullname']
+    print(f"Resolving link for module: {module}, fullname: {fullname}")
+    # 构建GitHub链接或本地文件路径
+    # 这里替换为你的GitHub仓库URL或自定义路径生成逻辑
+    return f"https://github.com/GMC-DRL/MetaBox/blob/Final/{module.replace('.', '/')}.py"
 autodoc2_packages = [
     {
         "path": "../../src",
@@ -61,15 +91,24 @@ exclude_patterns = []
 
 # 添加 MyST 解析器配置
 myst_enable_extensions = [
-    "colon_fence"
+    "colon_fence",
+    "dollarmath",
+    "fieldlist",
+    "linkify",
 ]
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "shibuya"
+
 # html_theme = 'sphinx_book_theme'
 # html_theme = 'press'
 html_static_path = ['_static']
 locale_dirs = ["locale/"]
 gettext_compact = "docs"
+autosummary_generate = True
+autosummary_imported_members = True
+nb_execution_mode = "off"
+nbsphinx_execute = "never"
+nb_output_stderr = "remove"
